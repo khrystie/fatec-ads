d3ca561e56228b4d43af9315a8d112878b0392f8
khrystie <86361095+khrystie@users.noreply.github.com>
2021-07-03 22:52:40 -0300

    Create L01_ex02.cpp

---
+++ ALP/sequencia/L01_ex02.cpp
@@ -0,0 +1,21 @@
+/* 
+    Exercício: Faça um algoritmo que receba 2 números inteiros e mostre os dois números.
+*/
+
+#include <iostream>
+
+int main() {
+  int num1=0, num2=0;
+
+  std::cout << "Mostra dois números digitados\n";
+
+  std::cout <<"Digite o primeiro número inteiro: ";
+  std::cin >> num1;
+
+  std::cout <<"Digite o segundo número inteiro: ";
+  std::cin >> num2;
+
+  std::cout <<"O primeiro número digitado é: " <<num1 <<"\n";
+  std::cout <<"O segundo número digitado é: " <<num2;
+}
+
