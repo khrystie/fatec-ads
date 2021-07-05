4e951d10256872fdce8a11ca6c0a01d54c3b4657
khrystie <86361095+khrystie@users.noreply.github.com>
2021-07-05 14:27:12 -0300

    Create L03_ex01.cpp

---
+++ ALP/lacos/L03_ex01.cpp
@@ -0,0 +1,19 @@
+/*
+    1-	Mostrar o intervalo de inteiros de 0 a 100
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int cont=0;
+
+  cout <<"Mostra de 0 a 100";
+
+  while ( cont <=100) {
+    cout << cont;
+    cont = cont +1 ;
+  }
+
+}
