1a25ba9f6ff1a87410d083178be45362f40e7507
khrystie <akemi-kurashima@hotmail.com>
2022-03-05 19:16:46 -0300

    reescrevendo código - personagens colidem?

--- ESW2/Aula 3 /retangulo.js
+++ ESW2/Aula 3 /retangulo.js
@@ -1,32 +1,35 @@
-const retangulo1 = {
-    x1:1,
-    y1:1, 
-    x2:5, 
-    y2:5
+const retanguloEnvolventePersonagem1 = {
+    x1: 1,
+    y1: 1,
+    x2: 5,
+    y2: 5
 }
-const retangulo2 = {
-    x1:3, 
-    y1:3, 
-    x2:7, 
-    y2:7
+const retanguloEnvolventePersonagem2 = {
+    x1: 3,
+    y1: 3,
+    x2: 7,
+    y2: 7
 }
 
-const retanguloInterseccao = calcularRetanguloDeInterseccaoEntreRetangulos(retangulo1, retangulo2);
-
-console.log(calcularAreaRetangulo(retanguloInterseccao));
+console.log(houveColisao(retanguloEnvolventePersonagem1, retanguloEnvolventePersonagem2));
 
 //...
 
-function calcularAreaRetangulo(retangulo) {
-    if(retangulo.x1 >= retangulo.x2 || retangulo.y1>= retangulo.y2) console.log(0);
-    return console.log((retangulo.x2-retangulo.x1)*(retangulo.y2-retangulo.y1));
+function houveColisao(retanguloEnvolventePersonagem1, retanguloEnvolventePersonagem2) {
+    return calcularAreaRetangulo(calcularRetanguloDeInterseccaoEntre(retanguloEnvolventePersonagem1, retanguloEnvolventePersonagem2)) > 0;
 }
 
-function calcularRetanguloDeInterseccaoEntreRetangulos(retangulo1, retangulo2){
-    return {      
-        x1: Math.max(retangulo1.x1,retangulo2.x1),
-        y1: Math.max(retangulo1.y1,retangulo2.y1), 
-        x2: Math.min(retangulo1.x2,retangulo2.x2),
-        y2: Math.min(retangulo1.y2,retangulo2.y2)  
+function calcularAreaRetangulo(retangulo) {
+    if (retangulo.x1 >= retangulo.x2 || retangulo.y1 >= retangulo.y2) return 0;
+    return (retangulo.x2 - retangulo.x1) * (retangulo.y2 - retangulo.y1);
+}
+
+function calcularRetanguloDeInterseccaoEntre(retanguloEnvolventePersonagem1, retanguloEnvolventePersonagem2) {
+    return {
+        x1: Math.max(retanguloEnvolventePersonagem1.x1, retanguloEnvolventePersonagem2.x1),
+        y1: Math.max(retanguloEnvolventePersonagem1.y1, retanguloEnvolventePersonagem2.y1),
+        x2: Math.min(retanguloEnvolventePersonagem1.x2, retanguloEnvolventePersonagem2.x2),
+        y2: Math.min(retanguloEnvolventePersonagem1.y2, retanguloEnvolventePersonagem2.y2)
     }
 }
+
