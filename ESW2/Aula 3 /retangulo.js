ed46850bf288acc575405ff4d987aaa1cdfd71cf
khrystie <akemi-kurashima@hotmail.com>
2022-03-05 17:44:08 -0300

    reescrevendo o código - melhor leitura e aproveitamento de código

--- ESW2/Aula 3 /retangulo.js
+++ ESW2/Aula 3 /retangulo.js
@@ -1,13 +1,32 @@
+const retangulo1 = {
+    x1:1,
+    y1:1, 
+    x2:5, 
+    y2:5
+}
+const retangulo2 = {
+    x1:3, 
+    y1:3, 
+    x2:7, 
+    y2:7
+}
 
-/* plano cartesiano */
-const ax1=1, ay1=1, ax2=5, ay2=6;
-const bx1=3, by1=3, bx2=7, by2=7;
+const retanguloInterseccao = calcularRetanguloDeInterseccaoEntreRetangulos(retangulo1, retangulo2);
 
-const maxX1 = Math.max(ax1,bx1); //3
-const maxY1 = Math.max(ay1,by1); //3
-const minX2 = Math.min(ax2,bx2); //5
-const minY2 = Math.min(ay2,by2);// 6
+console.log(calcularAreaRetangulo(retanguloInterseccao));
 
-if(maxX1 >= minX2 || maxY1>= minY2) console.log(0);
-else console.log((minX2-maxX1)*(minY2-maxY1));
+//...
 
+function calcularAreaRetangulo(retangulo) {
+    if(retangulo.x1 >= retangulo.x2 || retangulo.y1>= retangulo.y2) console.log(0);
+    return console.log((retangulo.x2-retangulo.x1)*(retangulo.y2-retangulo.y1));
+}
+
+function calcularRetanguloDeInterseccaoEntreRetangulos(retangulo1, retangulo2){
+    return {      
+        x1: Math.max(retangulo1.x1,retangulo2.x1),
+        y1: Math.max(retangulo1.y1,retangulo2.y1), 
+        x2: Math.min(retangulo1.x2,retangulo2.x2),
+        y2: Math.min(retangulo1.y2,retangulo2.y2)  
+    }
+}
